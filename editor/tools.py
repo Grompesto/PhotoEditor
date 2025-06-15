af0673b925b46b8e48b8681d702938a7d4fa0442
Grompesto <lordgromp0806@gmail.com>
2025-06-15 14:16:33 +0200

    Final changes in project

--- editor/tools.py
+++ editor/tools.py
@@ -1,15 +1,19 @@
 from PIL import ImageFilter, ImageEnhance, ImageDraw, ImageFont
 from tkinter.simpledialog import askstring
 
-#create draw on canvas tools
-class DrawTools:
+class Tools:
+    def __init__(self, canvas, image_ref, image_pos, update_image_callback):
+        self._canvas = canvas
+        self._image_ref = image_ref
+        self._image_pos = image_pos
+        self._update_image = update_image_callback
+
+#create classes for drawing on canvas tools
+class DrawTools(Tools):
     def __init__(self, canvas, image_ref, image_pos, update_image_callback, colour, size=1):
-        self.canvas = canvas
+        super().__init__(canvas, image_ref, image_pos, update_image_callback)
         self.colour = colour
         self.size = size
-        self.image_ref = image_ref
-        self.image_pos = image_pos
-        self.update_image_callback = update_image_callback
 
     def set_colour(self, new_colour):
         self.colour = new_colour
@@ -17,96 +21,96 @@ class DrawTools:
     def set_size(self, new_size):
         self.size = new_size
 
-class BrushTool(DrawTools):
-    def use_tool(self, event):
-        x, y = event.x - self.image_pos[0], event.y - self.image_pos[1]
-        r = self.size
-        draw = ImageDraw.Draw(self.image_ref)
-        draw.ellipse((x - r, y - r, x + r, y + r), fill=self.colour)
-        self.update_image_callback()
-
-class OvalTool(DrawTools):
-    def use_tool(self,event):
-        x, y = event.x - self.image_pos[0], event.y - self.image_pos[1]
-        r = self.size
-        draw = ImageDraw.Draw(self.image_ref)
-        draw.ellipse((x - r, y - r, x + r, y + r), fill=self.colour)
-        self.update_image_callback()
-
-#create classes for tools which based on pillow library
-class PillowTools:
-    def __init__(self, image):
-        self.image = image
-
-#create blur tool
-class BlurTool(PillowTools):
-    def __init__(self, image, radius):
-        self.image = image
-        self.radius = radius
-
-    def use_tool(self):
-        return self.image.filter(ImageFilter.GaussianBlur(self.radius))
-
-#create contrast tool
-class ContrastTool(PillowTools):
-    def __init__(self, image, factor):
-        self.image = image
-        self.factor = factor
-
-    def use_tool(self):
-        enhancer = ImageEnhance.Contrast(self.image)
-        return enhancer.enhance(self.factor)
-
-#create move tool
-class MoveTool:
-    def __init__(self, canvas, image_id, image_pos):
-        self.canvas = canvas
-        self.image_id = image_id
-        self.image_pos = image_pos
-        self.start_x = 0
-        self.start_y = 0
-
-    def start_move(self, event):
-        self.start_x = event.x
-        self.start_y = event.y
-
-    def move_image(self, event):
-        dx = event.x - self.start_x
-        dy = event.y - self.start_y
-        self.canvas.move(self.image_id, dx, dy)
-        self.start_x = event.x
-        self.start_y = event.y
-        self.image_pos[0] += dx
-        self.image_pos[1] += dy
-
-#create rotate tool
-class RotateTool:
-    def __init__(self, image, angle):
-        self.image = image
-        self.angle = angle
-
-    def use_tool(self):
-        return self.image.rotate(self.angle, expand=True, fillcolor=('lightgray'))
-
 #create text tool
-class TextTool:
+class TextTool(DrawTools):
     def __init__(self, canvas, image_ref, image_pos, update_image_callback, colour='black', font_path=None, font_size=24):
-        self.canvas = canvas
-        self.image_ref = image_ref
-        self.image_pos = image_pos
-        self.update_image_callback = update_image_callback
-        self.colour = colour
-        self.font_size = font_size
+        super().__init__(canvas, image_ref, image_pos, update_image_callback, colour, font_size)
         self.font_path = font_path or "arial.ttf"
 
     def set_colour(self, new_colour):
         self.colour = new_colour
 
+    def set_size(self, new_size):
+        self.font_size = new_size
+
     def use_tool(self, event):
         text = askstring("Input text", "Input text on the image:")
-        draw = ImageDraw.Draw(self.image_ref)
+        draw = ImageDraw.Draw(self._image_ref)
         font = ImageFont.truetype(self.font_path, self.font_size)
-        x = event.x - self.image_pos[0]
-        y = event.y - self.image_pos[1]
+        x = event.x - self._image_pos[0]
+        y = event.y - self._image_pos[1]
         draw.text((x, y), text, font=font, fill=self.colour)
-        self.update_image_callback()
+        self._update_image()
+
+#create brush tool
+class BrushTool(DrawTools):
+    def use_tool(self, event):
+        x, y = event.x - self._image_pos[0], event.y - self._image_pos[1]
+        r = self.size
+        draw = ImageDraw.Draw(self._image_ref)
+        draw.ellipse((x - r, y - r, x + r, y + r), fill=self.colour)
+        self._update_image()
+
+#create oval tool
+class OvalTool(DrawTools):
+    def use_tool(self,event):
+        x, y = event.x - self._image_pos[0], event.y - self._image_pos[1]
+        r = self.size
+        draw = ImageDraw.Draw(self._image_ref)
+        draw.ellipse((x - r, y - r, x + r, y + r), fill=self.colour)
+        self._update_image()
+
+#create classes for tools which based on pillow library
+class PillowTools:
+    def __init__(self, image):
+        self._image = image
+
+#create blur tool
+class BlurTool(PillowTools):
+    def __init__(self, image, radius):
+        super().__init__(image)
+        self._radius = radius
+
+    def use_tool(self):
+        return self._image.filter(ImageFilter.GaussianBlur(self._radius))
+
+#create contrast tool
+class ContrastTool(PillowTools):
+    def __init__(self, image, factor):
+        super().__init__(image)
+        self._factor = factor
+
+    def use_tool(self):
+        enhancer = ImageEnhance.Contrast(self._image)
+        return enhancer.enhance(self._factor)
+
+#create move tool
+class MoveTool:
+    def __init__(self, canvas, image_id, image_pos):
+        self._canvas = canvas
+        self._image_id = image_id
+        self._image_pos = image_pos
+        self._start_x = 0
+        self._start_y = 0
+
+    def start_move(self, event):
+        self._start_x = event.x
+        self._start_y = event.y
+
+    def move_image(self, event):
+        dx = event.x - self._start_x
+        dy = event.y - self._start_y
+        self._canvas.move(self._image_id, dx, dy)
+        self._start_x = event.x
+        self._start_y = event.y
+        self._image_pos[0] += dx
+        self._image_pos[1] += dy
+
+#create rotate tool
+class RotateTool(PillowTools):
+    def __init__(self, image, angle):
+        super().__init__(image)
+        self._angle = angle
+
+    def use_tool(self):
+        return self._image.rotate(self._angle, expand=True, fillcolor='lightgray')
--- gui/interface.py
+++ gui/interface.py
@@ -1,8 +1,7 @@
 from tkinter import *
-from tkinter import filedialog
+from tkinter import filedialog, Scale, HORIZONTAL, messagebox
 from PIL import ImageTk, Image
-from editor.tools import DrawTools, OvalTool, BrushTool, PillowTools, BlurTool, ContrastTool, MoveTool, RotateTool, TextTool
-from tkinter import Scale, HORIZONTAL
+from editor.tools import OvalTool, BrushTool, BlurTool, ContrastTool, MoveTool, RotateTool, TextTool
 
 #create main window
 window = Tk()
@@ -16,7 +15,7 @@ window.iconphoto(True,icon)
 left_frame = Frame(window, bg="darkgray")
 left_frame.grid(row=0, column=0, rowspan=2, sticky="ns")
 
-# create frame with tools
+#create frame with tools
 tool_bar = Frame(left_frame, bg = 'gray',borderwidth=10)
 tool_bar.pack(side=TOP)
 
@@ -28,8 +27,6 @@ tool_brush = Button(tool_bar,text='Brush')
 tool_brush.pack()
 tool_move = Button(tool_bar,text='Move')
 tool_move.pack()
-tool_erasor = Button(tool_bar,text='Erasor')
-tool_erasor.pack()
 tool_contrast = Button(tool_bar,text='Contrast')
 tool_contrast.pack()
 tool_blur = Button(tool_bar,text='Blur')
@@ -56,9 +53,10 @@ color_black.pack()
 color_orange = Button(colorpalette_bar, bg='orange',command=lambda: select_colour('orange'))
 color_orange.pack()
 
-# area for image
+#area for image
 canvas = Canvas(window, width=1280, height=720, bg = 'lightgray')
 canvas.grid(row=0,column=1)
+
 current_image = None
 tk_image = None
 image_id = None
@@ -87,26 +85,31 @@ contrast_scale = Scale(right_frame, from_=0.1, to=5.0, resolution=0.1, orient=HO
 contrast_scale.set(0)
 contrast_scale.pack(pady=20, padx=10, fill="x")
 
-# scale for tools size
+#scale for tools size
 tool_size_scale = Scale(right_frame, from_=1, to=100, orient=HORIZONTAL, label="Tool size", command=lambda val: select_size(int(val)))
 tool_size_scale.set(24)
 tool_size_scale.pack(pady=20, padx=10, fill="x")
 
-# function to load image through menubar->file->open...
+#function to load image through menubar->file->open...
 def image_open():
     global current_image, tk_image, image_id, image_pos
-    fileTypes = [('Image Files','*.png;*.jpg;*.jpeg')]
-    path = filedialog.askopenfilename(filetypes=fileTypes)
+    file_types = [('Image Files','*.png;*.jpg;*.jpeg')]
+    path = filedialog.askopenfilename(filetypes=file_types)
     current_image = Image.open(path).resize((700,700))
     tk_image = ImageTk.PhotoImage(current_image)
     canvas.delete("all")
     image_pos = [0, 0]
     image_id = canvas.create_image(image_pos[0], image_pos[1], anchor=NW, image=tk_image)
+#error message if image isn't loaded
+def show_error():
+    messagebox.showwarning("No image", "Please load an image.")
 
-# function to save image through menubar->file->save as...
+#function to save image through menubar->file->save as...
 def image_save_as():
-    fileTypes = [('Image Files', '*.png;*.jpg;*.jpeg')]
-    path = filedialog.asksaveasfilename(filetypes=fileTypes)
+    if current_image is None:
+        return
+    file_types = [('Image Files', '*.png;*.jpg;*.jpeg')]
+    path = filedialog.asksaveasfilename(filetypes=file_types)
     current_image.save(path)
 
 #function to update image after operations
@@ -116,12 +119,15 @@ def update_image():
     canvas.delete(image_id)
     image_id = canvas.create_image(image_pos[0], image_pos[1], anchor=NW, image=tk_image)
 
-# function for blank file through menubar->file->new file
+#function for blank file through menubar->file->new file
 def new_file():
+    global current_image, tk_image, image_id, history, redo_history
     canvas.delete("all")
-
-def undo_move():
-    pass
+    current_image = None
+    tk_image = None
+    image_id = None
+    history.clear()
+    redo_history.clear()
 
 #function to make step forward
 def stepf():
@@ -148,14 +154,16 @@ colour = 'black'
 def select_colour(new_colour):
     global colour
     colour = new_colour
-    current_tool.set_colour(colour)
+    if current_tool is not None:
+        current_tool.set_colour(colour)
 
 #allows to select size for draw tools
 size = 1
 def select_size(new_size):
     global size
     size = new_size
-    current_tool.set_size(size)
+    if current_tool is not None:
+        current_tool.set_size(size)
 
 #allows to select tools
 current_tool = None
@@ -163,14 +171,21 @@ active_button = None
 
 #function to deselect tool
 def deselect_tool():
-    global active_button
-    canvas.unbind(active_button)
+    global active_button, current_tool
+    if active_button:
+        canvas.unbind(active_button)
     active_button = None
+    current_tool = None
 
 #function for brush tool
 def select_brush():
     global current_tool, active_button
     deselect_tool()
+    if not current_image:
+        show_error()
+        return
+    history.append(current_image.copy())
+    redo_history.clear()
     current_tool = BrushTool(canvas, current_image, image_pos, update_image, colour, size)
     canvas.bind('<B1-Motion>', current_tool.use_tool)
     active_button = '<B1-Motion>'
@@ -179,6 +194,11 @@ def select_brush():
 def select_oval():
     global current_tool, active_button
     deselect_tool()
+    if not current_image:
+        show_error()
+        return
+    history.append(current_image.copy())
+    redo_history.clear()
     current_tool = OvalTool(canvas, current_image, image_pos, update_image, colour, size)
     canvas.bind('<Button-1>', current_tool.use_tool)
     active_button = '<Button-1>'
@@ -187,6 +207,9 @@ def select_oval():
 def select_move():
     global current_tool, active_button,image_id, image_pos
     deselect_tool()
+    if not current_image:
+        show_error()
+        return
     current_tool = MoveTool(canvas, image_id, image_pos)
     canvas.bind('<Button-1>', current_tool.start_move)
     canvas.bind('<B1-Motion>', current_tool.move_image)
@@ -195,46 +218,57 @@ def select_move():
 #function for rotate tool
 def select_rotate():
     global current_image, tk_image, history, redo_history, image_id, image_pos
-    if current_image:
-        history.append(current_image.copy())
-        redo_history.clear()
-        angle = rotate_scale.get()
-        rotate_tool = RotateTool(current_image, angle)
-        current_image = rotate_tool.use_tool()
-        tk_image = ImageTk.PhotoImage(current_image)
-        canvas.delete(image_id)
-        image_id = canvas.create_image(image_pos[0], image_pos[1], anchor=NW, image=tk_image)
+    if not current_image:
+        show_error()
+        return
+    history.append(current_image.copy())
+    redo_history.clear()
+    angle = rotate_scale.get()
+    rotate_tool = RotateTool(current_image, angle)
+    current_image = rotate_tool.use_tool()
+    tk_image = ImageTk.PhotoImage(current_image)
+    canvas.delete(image_id)
+    image_id = canvas.create_image(image_pos[0], image_pos[1], anchor=NW, image=tk_image)
 
 #function for blur tool
 def select_blur():
     global current_image, tk_image, history, redo_history, image_id, image_pos
-    if current_image:
-        history.append(current_image.copy())
-        redo_history.clear()
-        radius = blur_scale.get()
-        blur_tool = BlurTool(current_image, radius)
-        current_image = blur_tool.use_tool()
-        tk_image = ImageTk.PhotoImage(current_image)
-        canvas.delete(image_id)
-        image_id = canvas.create_image(image_pos[0], image_pos[1], anchor=NW, image=tk_image)
+    if not current_image:
+        show_error()
+        return
+    history.append(current_image.copy())
+    redo_history.clear()
+    radius = blur_scale.get()
+    blur_tool = BlurTool(current_image, radius)
+    current_image = blur_tool.use_tool()
+    tk_image = ImageTk.PhotoImage(current_image)
+    canvas.delete(image_id)
+    image_id = canvas.create_image(image_pos[0], image_pos[1], anchor=NW, image=tk_image)
 
 #function for contrast tool
 def select_contrast():
     global current_image, tk_image, history, redo_history, image_id, image_pos
-    if current_image:
-        history.append(current_image.copy())
-        redo_history.clear()
-        factor = contrast_scale.get()
-        contrast_tool = ContrastTool(current_image, factor)
-        current_image = contrast_tool.use_tool()
-        tk_image = ImageTk.PhotoImage(current_image)
-        canvas.delete(image_id)
-        image_id = canvas.create_image(image_pos[0], image_pos[1], anchor=NW, image=tk_image)
+    if not current_image:
+        show_error()
+        return
+    history.append(current_image.copy())
+    redo_history.clear()
+    factor = contrast_scale.get()
+    contrast_tool = ContrastTool(current_image, factor)
+    current_image = contrast_tool.use_tool()
+    tk_image = ImageTk.PhotoImage(current_image)
+    canvas.delete(image_id)
+    image_id = canvas.create_image(image_pos[0], image_pos[1], anchor=NW, image=tk_image)
 
 #function for text tool
 def select_text():
     global current_tool, active_button
     deselect_tool()
+    if not current_image:
+        show_error()
+        return
+    history.append(current_image.copy())
+    redo_history.clear()
     font_size = tool_size_scale.get()
     current_tool = TextTool(canvas, current_image, image_pos, update_image, colour, font_size=font_size)
     canvas.bind('<Button-1>', current_tool.use_tool)
@@ -251,26 +285,13 @@ menubar.add_cascade(label = 'Edit', menu = edit)
 
 file.add_command(label='New file', command=new_file)
 file.add_command(label='Open...', command=image_open)
-file.add_command(label='Save...')
+file.add_command(label='Save...', command=image_save_as)
 file.add_command(label='Save as...', command=image_save_as)
 file.add_separator()
 file.add_command(label='Exit', command=window.quit)
 
-edit.add_command(label='Undo move',command=undo_move)
 edit.add_command(label='Step Forward', command=stepf)
 edit.add_command(label='Step Backward',command=stepb)
-edit.add_separator()
-edit.add_command(label='Cut')
-edit.add_command(label='Copy')
-edit.add_command(label='Paste')
-
-rightClick.add_command(label = 'Cut')
-rightClick.add_command(label='Copy')
-rightClick.add_command(label='Paste')
-
-# add popup menu when right-clicked
-def right_popup(event):
-    rightClick.tk_popup(event.x_root, event.y_root)
 
 tool_blur.config(command=select_blur)
 tool_contrast.config(command=select_contrast)
@@ -279,8 +300,6 @@ tool_brush.config(command=select_brush)
 tool_move.config(command=select_move)
 tool_rotate.config(command=select_rotate)
 tool_text.config(command=select_text)
-
-window.bind('<Button-3>',right_popup)
 
 window.config(menu = menubar)
 
